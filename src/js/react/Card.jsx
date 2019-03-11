78a0253588be2ffa75bde0f02777b429e07d2b54
bringmetheaugust <bringmetheaugust@gmail.com>
2019-03-11 09:51:41 +0200

    updated Card component methods

--- src/js/react/Card.jsx
+++ src/js/react/Card.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 export default class Card extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			isActive: false,
+			isDisabled: false
+		}
 	}
+	toActive = () => this.setState({isActive: true});
+	toDeactivate = () => setTimeout(() => this.setState({isActive: false}), 500);
+	toDisable = () => this.setState({isDisabled: true});
 	render(){
 		return(
-			<li className='card-wrap'>
+			<li onClick = {isDisabled ? false : this.toActive} className='card-wrap'>
+				{
+					isActive && <img src={require(`../../img/data/${this.props.img}`)}/>
+				}
 			</li>
 		)
 	}
