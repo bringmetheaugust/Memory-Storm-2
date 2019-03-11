c71bf09dfd8305a54023fa6ea71c3c670cc33c96
bringmetheaugust <bringmetheaugust@gmail.com>
2019-03-11 15:06:27 +0200

    disabled click event on Card until the game start

--- src/js/react/Card.jsx
+++ src/js/react/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export default class Card extends React.Component{
+class Card extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
@@ -12,9 +13,13 @@ export default class Card extends React.Component{
 	toDeactivate = () => setTimeout(() => this.setState({isActive: false}), 500);
 	toDisable = () => this.setState({isDisabled: true});
 	render = () =>
-		<li onClick = {this.state.isDisabled ? false : this.toActive} className='card-wrap'>
+		<li onClick = {(this.state.isDisabled || !this.props.store.play) ? null : this.toActive} className='card-wrap'>
 			{
 				this.state.isActive && <img src={this.props.img}/>
 			}
 		</li>
 }
+
+export default connect(
+	state => ({store: state})
+)(Card);
