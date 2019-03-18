06cd11f9f086f63e97cdc53cc9b95e9c9313e5be
bringmetheaugust <bringmetheaugust@gmail.com>
2019-03-18 13:05:14 +0200

    added splash

--- src/js/react/App.jsx
+++ src/js/react/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Game from './Game.jsx';
 import Settings from './Settings.jsx';
 import Alert from './Alert.jsx';
+import Splash from './Splash.jsx';
 import {connect} from 'react-redux';
 
 class App extends React.Component{
@@ -18,6 +19,7 @@ class App extends React.Component{
 	render(){
 		return(
 			<React.Fragment>
+				<Splash/>
 				<Game/>
 				<Settings/>
 				{
