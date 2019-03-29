08fa20e9acb2f7cc733909ce9da8cfb6097300de
bringmetheaugust <bringmetheaugust@gmail.com>
2019-03-29 13:14:54 +0200

    separated game time counter from <Game> for forbiding updating

--- src/js/react/Game.jsx
+++ src/js/react/Game.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import Card from './Card.jsx';
+import Counter from './Counter.jsx';
 import { connect } from 'react-redux';
 
 class Game extends React.Component {
 	constructor(props) {
 		super(props);
 	}
-	// shouldComponentUpdate(nextPr) {
-	// 	console.log(this.props.store.gameState.counter, nextPr.store.gameState.counter); 
-	// 	// return this.props.store.gameState.counter === nextPr.store.gameState.counter ? true : false;
-	// 	return true;
-	// }
 	render() {
-		const density = this.props.store.settings.density;
 		return(
 			<div className='game-field-wrap'>
 				<ul id='game-field'
-					className={this.props.store.gameState.play ? 'play' : ''}
-					style={{ gridTemplate : `repeat(${density}, 1fr)/repeat(${density}, 1fr)` }}
+					className={this.props.play ? 'play' : ''}
+					style={{ gridTemplate : `repeat(${this.props.density}, 1fr)/repeat(${this.props.density}, 1fr)` }}
 				>
-					{ this.props.cards.map(i => <Card key={Math.random()} data={i} />) }
+					{this.props.cards.map(i => <Card key={Math.random()} data={i} />)}
 				</ul>
-				<div className='count'>time left :
-					<div id='count'>{this.props.store.gameState.counter}</div>
-				</div>
+				<Counter />
 			</div>
-
 		)
 	}
 }
@@ -33,6 +25,7 @@ class Game extends React.Component {
 export default connect(
 	state => ({
 		cards : state.cards.sort(() => Math.random() - Math.random()),
-		store: state
+		density: state.settings.density,
+		play: state.gameState.play
 	}),
 )(Game);
